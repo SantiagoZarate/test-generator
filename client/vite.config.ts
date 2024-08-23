754f54e8a1db19419afc755dfc1c224af4867101
SantiagoZarate <santiago2001zarate@gmail.com>
2024-08-23 17:47:16 -0300

    test: add basic vitest setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
@@ -5,6 +7,9 @@ import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "happy-dom",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
