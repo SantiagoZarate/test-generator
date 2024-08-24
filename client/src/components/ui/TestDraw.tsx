7faecb2aaf038f88d03379704412633c476c695f
SantiagoZarate <santiago2001zarate@gmail.com>
2024-08-24 20:03:45 -0300

    style: add multiple choice css draw

---
+++ client/src/components/ui/TestDraw.tsx
@@ -0,0 +1,12 @@
+import "../../styles/test.css";
+
+export function TestDraw() {
+  return (
+    <div className="test">
+      <span />
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+}
