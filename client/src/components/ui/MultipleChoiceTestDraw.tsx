582acba241748178a6ba0535b1b30299df8a165b
SantiagoZarate <santiago2001zarate@gmail.com>
2024-08-25 18:37:31 -0300

    style: refine multiple choice draw

--- client/src/components/ui/MultipleChoiceTestDraw.tsx
+++ client/src/components/ui/MultipleChoiceTestDraw.tsx
@@ -1,19 +1,22 @@
 import "../../styles/multiple-choice-test.css";
+import { CrossMicroIcon } from "../icons/CrossMicroIcon";
 import { TickMicroIcon } from "../icons/TickMicroIcon";
 
 export function MultipleChoiceTestDraw() {
   return (
     <div className="multiple-choice-test">
-      <div className="question">
+      <div className="relative question">
+        <CrossMicroIcon className="absolute -translate-x-[2px]" />
         <span className="box" />
         <span className="answer" />
       </div>
       <div className="relative question">
-        <TickMicroIcon className="-translate-x-[2px]" />
-        {/* <span className="box" /> */}
+        <TickMicroIcon className="absolute -translate-x-[2px]" />
+        <span className="box" />
         <span className="answer" />
       </div>
-      <div className="question">
+      <div className="relative question">
+        <CrossMicroIcon className="absolute -translate-x-[2px]" />
         <span className="box" />
         <span className="answer" />
       </div>
