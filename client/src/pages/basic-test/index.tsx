c085e1c5bcc3f9ab728dfff77aa03deaeefe91b6
SantiagoZarate <santiago2001zarate@gmail.com>
2024-08-26 23:13:45 -0300

    test: show ai button when add 3 questions

--- client/src/pages/basic-test/index.tsx
+++ client/src/pages/basic-test/index.tsx
@@ -142,6 +142,7 @@ export function BasicTestPage() {
         {questions.length >= MIN_QUESTIONS_FOR_AI ? (
           <>
             <Button
+              data-testid="ai-button"
               disabled={loadingAI}
               onClick={handleGetAiSuggestions}
               className="inline-flex h-12 w-full items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition hover:-translate-y-1 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
