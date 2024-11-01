2e1a1aba4027dd8e3a2a58fe77522c249d33c2d9
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-01 19:01:09 -0300

    fix: use Link component on header

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { AcademicCapIcon } from '../icons/AcademicCapIcon';
 import './header.css';
 import { ThemeSwitcher } from './ThemeSwitcher';
@@ -6,10 +7,10 @@ export function Header() {
   return (
     <header className="header fixed top-0 z-50 w-full py-2 print:hidden">
       <section className="mx-auto flex w-full max-w-screen-lg justify-between">
-        <a href="/" className="flex items-center gap-2 divide-x p-2">
+        <Link to="/" className="flex items-center gap-2 divide-x p-2">
           <AcademicCapIcon />
           <p className="px-2 font-semibold capitalize">Test Builder</p>
-        </a>
+        </Link>
         <ThemeSwitcher />
       </section>
     </header>
