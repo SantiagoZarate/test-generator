6e8d772982784e6cc54aefd3d63cdf9efa22636e
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-01 17:34:29 -0300

    chore: add local sqlite db for dev mode

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from "drizzle-kit";
 import { envs } from "./config/envs";
 
+let dbCredentials;
+
+if (envs.MODE === "dev") {
+  dbCredentials = {
+    url: "file:./drizzle/local.db",
+  };
+} else {
+  dbCredentials = {
+    url: envs.DB_URL,
+    authToken: envs.DB_TOKEN,
+  };
+}
+
 export default defineConfig({
   dialect: "sqlite",
   schema: "./drizzle/schemas/*",
   out: "./drizzle/migrations",
   driver: "turso",
-  dbCredentials: {
-    url: envs.DB_URL,
-    authToken: envs.DB_TOKEN,
-  },
+  dbCredentials,
 });
