9ec1af0b1d6719a8e9289930477081c71bfbe67e
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-01 19:08:21 -0300

    refactor: split index.css into smaller files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
 import { mainRouter } from './router/MainRouter.tsx';
+import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
