811fd17b1505183f12df927622523e960b7b84a3
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-01 16:38:10 -0300

    feat: add multiple test schemas

--- server/drizzle/schemas/index.ts
+++ server/drizzle/schemas/index.ts
@@ -1,13 +1,29 @@
 import {
-  testSchema,
   questionRelations,
   questionSchema,
   testRelations,
+  testSchema,
 } from "./test.schema";
+
+import {
+  multipleChoiceQuestionRelations,
+  multipleChoiceQuestionSchema,
+  multipleChoiceTestRelations,
+  multipleChoiceTestSchema,
+  optionRelations,
+  optionSchema,
+} from "./multipleTest.schema";
 
 export default {
   testSchema,
   questionRelations,
   questionSchema,
   testRelations,
+  // Multiple Choice
+  multipleChoiceQuestionSchema,
+  multipleChoiceTestSchema,
+  optionRelations,
+  optionSchema,
+  multipleChoiceQuestionRelations,
+  multipleChoiceTestRelations,
 };
