8e3c439990244dbad1bd155274162b7bcb7817d3
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-02 17:11:07 -0300

    style: add user system theme

--- client/src/hooks/useTheme.ts
+++ client/src/hooks/useTheme.ts
@@ -1,25 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export function useTheme() {
-  const [isDark, setIsDark] = useState<boolean>(
-    localStorage.getItem("theme") !== undefined,
-  );
+  const getInitialTheme = () => {
+    // Check localStorage for user preference
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme !== null) {
+      return savedTheme === 'true';
+    }
+    // Check system preference if no preference is saved
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  };
+
+  const [isDark, setIsDark] = useState<boolean>(getInitialTheme);
 
   useEffect(() => {
     if (isDark) {
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add('dark');
     }
   }, []);
 
   const toggleTheme = () => {
-    document.documentElement.classList.toggle("dark");
+    document.documentElement.classList.toggle('dark');
     setIsDark(!isDark);
 
     if (isDark) {
-      localStorage.removeItem("theme");
+      localStorage.removeItem('theme');
     } else {
-      localStorage.setItem("theme", "true");
+      localStorage.setItem('theme', 'true');
     }
   };
 
