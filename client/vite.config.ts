acc7979205ad6c8f14f36f58019271f031f5d08d
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-02 16:10:10 -0300

    chore(client): add dotenv and add proxy server to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,31 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "node:path";
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+
+import { config } from 'dotenv';
+config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "happy-dom",
+    environment: 'happy-dom',
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@backend": path.resolve(__dirname, "../server/src/types/"),
+      '@': path.resolve(__dirname, './src'),
+      '@backend': path.resolve(__dirname, '../server/src/types/'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_TEST_API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
