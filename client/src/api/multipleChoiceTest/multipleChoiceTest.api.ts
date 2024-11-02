0e6cc77ef6109c59a8fc318acac3e6d343dbd9c4
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-02 01:55:51 -0300

    feat: add multiple choice test by page Component

--- client/src/api/multipleChoiceTest/multipleChoiceTest.api.ts
+++ client/src/api/multipleChoiceTest/multipleChoiceTest.api.ts
@@ -1,9 +1,10 @@
 import { envs } from '@/config/envs';
+import { MPTest } from './multipleChoiceTest.type';
 
 const ENDPOINT = envs.TEST_API_URL + '/api/multiple-choice-tests';
 
 export const multipleChoiceTestAPI = {
-  async getOne(id: string) {
+  async getOne(id: string): Promise<MPTest> {
     return fetch(ENDPOINT + '/' + id)
       .then((response) => response.json())
       .then((response) => response.data);
