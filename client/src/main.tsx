86a2131f5a4b63bb193c96fb5fc5820954529780
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-02 01:38:56 -0300

    chore: add tanstack react query to manage async states

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { mainRouter } from './router/MainRouter.tsx';
 import './styles/index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={mainRouter} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={mainRouter} />
+    </QueryClientProvider>
   </StrictMode>
 );
