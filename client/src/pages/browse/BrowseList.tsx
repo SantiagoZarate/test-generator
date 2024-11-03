48c23fd2fb8d96459cdce92da0ae094eaee49655
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-03 20:58:14 -0300

    style: add arrow up icon when hovering over browse list item

--- client/src/pages/browse/BrowseList.tsx
+++ client/src/pages/browse/BrowseList.tsx
@@ -1,4 +1,5 @@
 import { GetAllTests } from '@/api/interface';
+import { ArrowUpRigthMicroIcon } from '@/components/icons/ArrowUpRigthMicroIcon';
 import { MotionItem } from '@/components/motion/MotionItem';
 import { MotionList } from '@/components/motion/MotionList';
 import { Link } from 'react-router-dom';
@@ -11,12 +12,19 @@ export function BrowseList({ tests }: Props) {
   return (
     <MotionList className="flex flex-col divide-y divide-border">
       {tests.map((test) => (
-        <MotionItem className="py-1" key={test.id}>
+        <MotionItem className="group py-1" key={test.id}>
           <Link
             to={`/multiple-choice/${test.id}`}
             className="flex cursor-pointer items-center justify-between rounded-lg p-1 px-2 transition hover:bg-border"
           >
-            <p>{test.title}</p>
+            <section className="flex items-center gap-2">
+              <span className="text-red-50 opacity-0 transition group-hover:opacity-100">
+                <ArrowUpRigthMicroIcon />
+              </span>
+              <p className="-translate-x-6 transition group-hover:translate-x-0">
+                {test.title}
+              </p>
+            </section>
             <p>
               {test.questionsCounts}{' '}
               {test.questionsCounts > 1 ? 'preguntas' : 'pregunta'}
