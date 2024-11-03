067415cbb8a77be3c96588b5677aba9e3d4fa358
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-03 18:25:23 -0300

    feat(client): add link button to redirect user to multiple choice test by id

--- client/src/pages/browse/BrowseMultipleChoiceTestsPage.tsx
+++ client/src/pages/browse/BrowseMultipleChoiceTestsPage.tsx
@@ -1,4 +1,5 @@
 import { useGetMultipleChoiceTests } from '@/hooks/useGetMultipleChoiceTetst';
+import { Link } from 'react-router-dom';
 
 export function BrowseMultipleChoiceTestsPage() {
   const { data, isError, isLoading } = useGetMultipleChoiceTests();
@@ -11,10 +12,13 @@ export function BrowseMultipleChoiceTestsPage() {
         <ul className="flex flex-col divide-y divide-border">
           {data?.map((test) => (
             <li className="py-1" key={test.id}>
-              <section className="flex items-center justify-between rounded-lg p-1 px-2 transition hover:bg-border">
+              <Link
+                to={`/multiple-choice/${test.id}`}
+                className="flex items-center justify-between rounded-lg p-1 px-2 transition hover:bg-border"
+              >
                 <p>{test.title}</p>
                 <p>{test.questionsCounts} questions</p>
-              </section>
+              </Link>
             </li>
           ))}
         </ul>
