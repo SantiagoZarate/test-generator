e3d3a3b21727bef0b9e6716c238cfd1c020ac818
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-08 17:28:00 -0300

    fix(server): await for user service response

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -4,7 +4,7 @@ import { AuthRequest } from '../types/authRequest';
 
 class UserController {
   async getUserProfile(req: AuthRequest, res: Response) {
-    const data = userService.getUserProfile({ id: req.user!.id });
+    const data = await userService.getUserProfile({ id: req.user!.id });
     res.json({
       data,
       message: 'Retrieving user profile information',
