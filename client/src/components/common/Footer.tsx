23f271bd294271e9ffff94092ffcfe35716fc8ec
SantiagoZarate <santiago2001zarate@gmail.com>
2024-11-15 16:15:52 -0300

    style: add responsive design in footer and browse layout

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -3,18 +3,20 @@ import { FooterForm } from './FooterForm';
 
 export function Footer() {
   return (
-    <footer className="mx-auto flex w-full max-w-screen-lg items-start border-t border-neutral-600 px-4 pb-20 pt-6 print:hidden">
+    <footer className="mx-auto flex w-full max-w-screen-lg flex-col items-center justify-center gap-4 border-t border-neutral-600 px-4 pb-20 pt-6 sm:flex-row print:hidden">
       <section className="flex flex-1 items-center gap-1 text-neutral-400">
         <SparkelIcon />
-        made by{' '}
-        <a
-          target="_blank"
-          className="text-neutral-200 hover:underline"
-          href="https://www.github.com/SantiagoZarate"
-        >
-          Santiago Zarate
-        </a>{' '}
-        powered by Vite
+        <p>
+          made by{' '}
+          <a
+            target="_blank"
+            className="text-neutral-200 hover:underline"
+            href="https://www.github.com/SantiagoZarate"
+          >
+            Santiago Zarate
+          </a>{' '}
+          powered by Vite
+        </p>
       </section>
       <FooterForm />
     </footer>
--- client/src/layouts/BrowseLayout.tsx
+++ client/src/layouts/BrowseLayout.tsx
@@ -4,7 +4,7 @@ import { TestsTab } from './TestsTab';
 export function BrowseLayout() {
   return (
     <section className="flex flex-col gap-8">
-      <header className="flex items-center justify-between">
+      <header className="flex flex-col items-center sm:flex sm:flex-row sm:items-center sm:justify-between">
         <p>Browse over the lasts users tests</p>
         <TestsTab />
       </header>
