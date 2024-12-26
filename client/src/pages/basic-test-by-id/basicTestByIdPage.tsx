548d62327c73b1d8c1a301c2bbf3ed69e39162dd
SantiagoZarate <santiago2001zarate@gmail.com>
2024-12-26 20:28:05 -0300

    feat(client): add inputs and submit button to basic test

--- client/src/pages/basic-test-by-id/basicTestByIdPage.tsx
+++ client/src/pages/basic-test-by-id/basicTestByIdPage.tsx
@@ -1,13 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { testAPI } from '@/api/test/test.api';
-import { Item } from '@/components/ui/Item';
-import { List } from '@/components/ui/List';
+import { Button } from '@/components/ui/Button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/Input';
 import { PrintButton } from '@/components/ui/PrintButton';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useRef } from 'react';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
+import { z } from 'zod';
+
+const basicTestSchema = z.object({
+  answers: z.array(z.string().min(1, "answer can't be empty")),
+});
+
+type BasicTestSchema = z.infer<typeof basicTestSchema>;
 
 export function BasicTestByIdPage() {
   const { id } = useParams();
-
+  const prevAnswers = useRef('');
   const {
     data: test,
     isLoading,
@@ -16,6 +34,29 @@ export function BasicTestByIdPage() {
     queryKey: ['basic-test', id],
     queryFn: () => testAPI.getById({ id: id! }),
   });
+
+  const formState = useForm<BasicTestSchema>({
+    resolver: zodResolver(basicTestSchema),
+  });
+
+  // Set async default value
+  useEffect(() => {
+    formState.setValue('answers', test?.questions.map((_) => '') ?? []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [test]);
+
+  const handleSubmitTest = (data: BasicTestSchema) => {
+    console.log({ data });
+    prevAnswers.current = data.answers.toString();
+  };
+
+  const handleResetTest = () => {
+    prevAnswers.current = '';
+    formState.setValue(
+      'answers',
+      test!.questions.map((_) => '')
+    );
+  };
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -31,9 +72,47 @@ export function BasicTestByIdPage() {
         <h2 className="text-2xl font-semibold">{test?.title}</h2>
         <p>{test?.created_at}</p>
       </header>
-      <List>{test?.questions.map((q) => <Item>{q}</Item>)}</List>
-      <footer className="flex justify-end print:hidden">
+      <Form {...formState}>
+        <form
+          onSubmit={formState.handleSubmit(handleSubmitTest)}
+          className="flex flex-col gap-12"
+        >
+          {test?.questions.map((question, index) => (
+            <FormField
+              key={index}
+              name={`answers.${index}`}
+              control={formState.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="flex flex-col gap-2">
+                    {index + 1} - {question}
+                    <Input
+                      placeholder={`${index + 1} - answer`}
+                      className="border-b border-border bg-transparent"
+                      type="text"
+                      {...field}
+                    />
+                  </FormLabel>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button
+            disabled={
+              formState
+                .watch('answers')
+                .some((answer) => answer.length === 0) ||
+              formState.watch('answers').toString() === prevAnswers.current
+            }
+          >
+            Submit
+          </Button>
+        </form>
+      </Form>
+      <footer className="flex justify-end gap-2 print:hidden">
         <PrintButton />
+        <Button onClick={handleResetTest}>Reset test</Button>
       </footer>
     </>
   );
